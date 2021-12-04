5c14bdc5eaa5d993fdbbd68b229bd81dc267b911
Archisman Karmakar <92569441+ArchismanKarmakar@users.noreply.github.com>
2021-12-04 09:18:07 +0530

    Add files via upload

---
+++ PassVault/My Project/AssemblyInfo.vb
@@ -0,0 +1,37 @@
+﻿Imports System.Resources
+Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("PassVault")>
+<Assembly: AssemblyDescription("PassVault is a open source program developed for storage of user credentials safely with AES encryption. Developed by Blasteoroid Corporation.")>
+<Assembly: AssemblyCompany("Blasteoroid Corporation")>
+<Assembly: AssemblyProduct("PassVault")>
+<Assembly: AssemblyCopyright("Copyright ©  2021")>
+<Assembly: AssemblyTrademark("Blasteoroid Corporation")>
+
+<Assembly: ComVisible(True)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("031597c4-c4ac-4546-ac6b-9547f017ad2a")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.2.2.8")>
+<Assembly: AssemblyFileVersion("1.5.1.0")>
+<Assembly: NeutralResourcesLanguage("en")>
